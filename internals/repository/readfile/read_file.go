35d457df1ba0c8820a1258854792d6903f1577dc
Bagus Andrian <bagusandrian@Baguss-Laptop.local>
2024-09-01 01:32:42 +0700

    fixing generate mock

--- internals/repository/readfile/read_file.go
+++ internals/repository/readfile/read_file.go
@@ -2,7 +2,7 @@ package readfile
 
 import "github.com/bagusandrian/reconciliation-service/internals/model"
 
-//go:generate mockery --name=Handler --filename=mock_handler.go --inpackage
+//go:generate mockery --name=ReadFile --filename=mock_read_file.go --inpackage
 type ReadFile interface {
 	GetSystemReconciliationCSV(req model.ReconciliationRequest) (resp model.DataSystem, err error)
 	GetBankReconciliationCSV(req model.ReconciliationRequest) (resp model.DataBank, err error)
